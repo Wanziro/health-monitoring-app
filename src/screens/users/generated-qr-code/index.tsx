b5f9bc8b166f8aa015e1da8e5ea1e3b471f3fee7
Fabrice Niyongabo <ninga.fab@gmail.com>
2023-02-02 17:18:17 +0200

    qr codes

---
+++ src/screens/users/generated-qr-code/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function GeneratedQRCode() {
+  return (
+    <View>
+      <Text>GeneratedQRCode</Text>
+    </View>
+  );
+}
+
+export default GeneratedQRCode;
---
+++ src/screens/users/scan-qr-code/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function ScanQRCode() {
+  return (
+    <View>
+      <Text>scan qr code</Text>
+    </View>
+  );
+}
+
+export default ScanQRCode;
