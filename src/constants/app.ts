dfcf77e231710b1f71e8fd79ae05a53e68a844cb
Fabrice Niyongabo <ninga.fab@gmail.com>
2023-02-02 05:28:45 +0200

    created signed key

--- src/constants/app.ts
+++ src/constants/app.ts
@@ -1,7 +1,7 @@
 import {IAppConfig} from '../interfaces';
 
-const APP_MODE = 'dev';
-// const APP_MODE = 'production';
+// const APP_MODE = 'dev';
+const APP_MODE = 'production';
 const CONFIG = {
   dev: {
     // backendUrl: 'http://192.168.43.3:8080/api',
@@ -13,10 +13,10 @@ const CONFIG = {
     imageUploadUrl: 'http://172.31.34.64/RN/medequip/upload.php',
   },
   production: {
-    // backendUrl: 'https://medequip-backend.herokuapp.com/api',
-    backendUrl: 'https://mobile-mers-backend.onrender.com/api',
-    imageUrl: 'https://autoexpertsrwanda.com/images/ic2/uploads/',
-    imageUploadUrl: 'https://autoexpertsrwanda.com/images/ic2/upload.php',
+    backendUrl: 'https://health-monitoring-app.onrender.com/api',
+    imageUrl: 'https://lawyersofhope.org.rw/assets/images/controller/uploads/',
+    imageUploadUrl:
+      'https://lawyersofhope.org.rw/assets/images/controller/upload.php',
   },
 };
 
