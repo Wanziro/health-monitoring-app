c580e6a67026b70924960ff8b09043cf1c17dcdd
Fabrice Niyongabo <ninga.fab@gmail.com>
2023-10-02 15:42:02 +0200

    patients list fix

--- src/screens/nurses/add-patients/index.tsx
+++ src/screens/nurses/add-patients/index.tsx
@@ -18,12 +18,7 @@ import FullPageLoader from '../../full-page-loader';
 import axios from 'axios';
 import {app} from '../../../constants/app';
 import {useSelector} from 'react-redux';
-import {
-  errorHandler,
-  errorHandler2,
-  toastMessage,
-  toastMessage2,
-} from '../../../helpers';
+import {errorHandler2, toastMessage2} from '../../../helpers';
 import {RootState} from '../../../reducers';
 import {IBed} from '../../../../interfaces';
 const initialState: IPatientRegisterRequest = {
@@ -69,19 +64,64 @@ function AddPatients({navigation}: INavigationProp) {
     <KeyboardAvoidingView style={{flex: 1}}>
       <View
         style={{
-          flex: 1,
           padding: 10,
-          alignItems: 'center',
-          justifyContent: 'center',
           backgroundColor: appColors.BACKGROUND_COLOR,
         }}>
-        <View style={{width: '100%'}}>
-          <ScrollView>
-            <Picker
-              selectedValue={state.departmentId}
-              onValueChange={(itemValue, itemIndex) =>
-                setState({...state, departmentId: itemValue})
-              }
+        <ScrollView>
+          <Picker
+            selectedValue={state.departmentId}
+            onValueChange={(itemValue, itemIndex) =>
+              setState({...state, departmentId: itemValue})
+            }
+            style={{
+              backgroundColor: appColors.WHITE,
+              marginTop: 10,
+              borderRadius: 5,
+              padding: 10,
+              borderWidth: 1,
+              borderColor: appColors.BORDER_COLOR,
+            }}>
+            {[
+              {
+                _id: '',
+                name: 'Choose Department',
+                numberOfBeds: '',
+                type: '',
+                createdAt: '',
+              },
+              ...departments,
+            ].map((model, i) => (
+              <Picker.Item key={i} label={model.name} value={model._id} />
+            ))}
+          </Picker>
+          <Picker
+            selectedValue={state.bedId}
+            onValueChange={(itemValue, itemIndex) =>
+              setState({...state, bedId: itemValue})
+            }
+            style={{
+              backgroundColor: appColors.WHITE,
+              marginTop: 10,
+              borderRadius: 5,
+              padding: 10,
+              borderWidth: 1,
+              borderColor: appColors.BORDER_COLOR,
+            }}>
+            {[
+              {
+                _id: '',
+                bedNumber: 'Choose Bed',
+                departmentId: '',
+                createdAt: '',
+              },
+              ...bedsList,
+            ].map((model, i) => (
+              <Picker.Item key={i} label={model.bedNumber} value={model._id} />
+            ))}
+          </Picker>
+          <View style={{marginVertical: 10}}>
+            <Text style={{color: appColors.FOOTER_BODY_TEXT_COLOR}}>Names</Text>
+            <TextInput
               style={{
                 backgroundColor: appColors.WHITE,
                 marginTop: 10,
@@ -89,25 +129,17 @@ function AddPatients({navigation}: INavigationProp) {
                 padding: 10,
                 borderWidth: 1,
                 borderColor: appColors.BORDER_COLOR,
-              }}>
-              {[
-                {
-                  _id: '',
-                  name: 'Choose Department',
-                  numberOfBeds: '',
-                  type: '',
-                  createdAt: '',
-                },
-                ...departments,
-              ].map((model, i) => (
-                <Picker.Item key={i} label={model.name} value={model._id} />
-              ))}
-            </Picker>
-            <Picker
-              selectedValue={state.bedId}
-              onValueChange={(itemValue, itemIndex) =>
-                setState({...state, bedId: itemValue})
-              }
+                color: appColors.BLACK,
+              }}
+              placeholder="Patient Names"
+              onChangeText={(text: string) => setState({...state, names: text})}
+              value={state.names}
+            />
+          </View>
+          <View style={{marginVertical: 10}}>
+            <Text style={{color: appColors.FOOTER_BODY_TEXT_COLOR}}>Ages</Text>
+            <TextInput
+              keyboardType="number-pad"
               style={{
                 backgroundColor: appColors.WHITE,
                 marginTop: 10,
@@ -115,115 +147,21 @@ function AddPatients({navigation}: INavigationProp) {
                 padding: 10,
                 borderWidth: 1,
                 borderColor: appColors.BORDER_COLOR,
-              }}>
-              {[
-                {
-                  _id: '',
-                  bedNumber: 'Choose Bed',
-                  departmentId: '',
-                  createdAt: '',
-                },
-                ...bedsList,
-              ].map((model, i) => (
-                <Picker.Item
-                  key={i}
-                  label={model.bedNumber}
-                  value={model._id}
-                />
-              ))}
-            </Picker>
-            <View style={{marginVertical: 10}}>
-              <Text style={{color: appColors.FOOTER_BODY_TEXT_COLOR}}>
-                Names
-              </Text>
-              <TextInput
-                style={{
-                  backgroundColor: appColors.WHITE,
-                  marginTop: 10,
-                  borderRadius: 5,
-                  padding: 10,
-                  borderWidth: 1,
-                  borderColor: appColors.BORDER_COLOR,
-                  color: appColors.BLACK,
-                }}
-                placeholder="Patient Names"
-                onChangeText={(text: string) =>
-                  setState({...state, names: text})
-                }
-                value={state.names}
-              />
-            </View>
-            <View style={{marginVertical: 10}}>
-              <Text style={{color: appColors.FOOTER_BODY_TEXT_COLOR}}>
-                Ages
-              </Text>
-              <TextInput
-                keyboardType="number-pad"
-                style={{
-                  backgroundColor: appColors.WHITE,
-                  marginTop: 10,
-                  borderRadius: 5,
-                  padding: 10,
-                  borderWidth: 1,
-                  borderColor: appColors.BORDER_COLOR,
-                  color: appColors.BLACK,
-                }}
-                placeholder="Patient ages"
-                onChangeText={(text: string) =>
-                  setState({...state, ages: Number(text)})
-                }
-                value={state.ages.toString()}
-              />
-            </View>
-            <View style={{marginVertical: 10}}>
-              <Text style={{color: appColors.FOOTER_BODY_TEXT_COLOR}}>
-                Height
-              </Text>
-              <TextInput
-                keyboardType="number-pad"
-                style={{
-                  backgroundColor: appColors.WHITE,
-                  marginTop: 10,
-                  borderRadius: 5,
-                  padding: 10,
-                  borderWidth: 1,
-                  borderColor: appColors.BORDER_COLOR,
-                  color: appColors.BLACK,
-                }}
-                placeholder="Patient's Height"
-                onChangeText={(text: string) =>
-                  setState({...state, height: Number(text)})
-                }
-                value={state.height.toString()}
-              />
-            </View>
-            <View style={{marginVertical: 10}}>
-              <Text style={{color: appColors.FOOTER_BODY_TEXT_COLOR}}>
-                Weight
-              </Text>
-              <TextInput
-                keyboardType="number-pad"
-                style={{
-                  backgroundColor: appColors.WHITE,
-                  marginTop: 10,
-                  borderRadius: 5,
-                  padding: 10,
-                  borderWidth: 1,
-                  borderColor: appColors.BORDER_COLOR,
-                  color: appColors.BLACK,
-                }}
-                placeholder="Patient's weight"
-                onChangeText={(text: string) =>
-                  setState({...state, weight: Number(text)})
-                }
-                value={state.weight.toString()}
-              />
-            </View>
-            <Picker
-              selectedValue={state.sex}
-              onValueChange={(itemValue, itemIndex) =>
-                setState({...state, sex: itemValue})
+                color: appColors.BLACK,
+              }}
+              placeholder="Patient ages"
+              onChangeText={(text: string) =>
+                setState({...state, ages: Number(text)})
               }
+              value={state.ages.toString()}
+            />
+          </View>
+          <View style={{marginVertical: 10}}>
+            <Text style={{color: appColors.FOOTER_BODY_TEXT_COLOR}}>
+              Height
+            </Text>
+            <TextInput
+              keyboardType="number-pad"
               style={{
                 backgroundColor: appColors.WHITE,
                 marginTop: 10,
@@ -231,87 +169,127 @@ function AddPatients({navigation}: INavigationProp) {
                 padding: 10,
                 borderWidth: 1,
                 borderColor: appColors.BORDER_COLOR,
-              }}>
-              {[
-                {name: 'Sex', value: ''},
-                {name: 'Male', value: 'Male'},
-                {name: 'Female', value: 'Female'},
-              ].map((model, i) => (
-                <Picker.Item key={i} label={model.name} value={model.value} />
-              ))}
-            </Picker>
-            <View style={{marginVertical: 10}}>
-              <Text style={{color: appColors.FOOTER_BODY_TEXT_COLOR}}>
-                Medication
-              </Text>
-              <TextInput
-                style={{
-                  backgroundColor: appColors.WHITE,
-                  marginTop: 10,
-                  borderRadius: 5,
-                  padding: 10,
-                  borderWidth: 1,
-                  borderColor: appColors.BORDER_COLOR,
-                  color: appColors.BLACK,
-                }}
-                placeholder="Enter medication"
-                multiline={true}
-                onChangeText={(text: string) =>
-                  setState({...state, medication: text})
-                }
-                value={state.medication}
-              />
+                color: appColors.BLACK,
+              }}
+              placeholder="Patient's Height"
+              onChangeText={(text: string) =>
+                setState({...state, height: Number(text)})
+              }
+              value={state.height.toString()}
+            />
+          </View>
+          <View style={{marginVertical: 10}}>
+            <Text style={{color: appColors.FOOTER_BODY_TEXT_COLOR}}>
+              Weight
+            </Text>
+            <TextInput
+              keyboardType="number-pad"
+              style={{
+                backgroundColor: appColors.WHITE,
+                marginTop: 10,
+                borderRadius: 5,
+                padding: 10,
+                borderWidth: 1,
+                borderColor: appColors.BORDER_COLOR,
+                color: appColors.BLACK,
+              }}
+              placeholder="Patient's weight"
+              onChangeText={(text: string) =>
+                setState({...state, weight: Number(text)})
+              }
+              value={state.weight.toString()}
+            />
+          </View>
+          <Picker
+            selectedValue={state.sex}
+            onValueChange={(itemValue, itemIndex) =>
+              setState({...state, sex: itemValue})
+            }
+            style={{
+              backgroundColor: appColors.WHITE,
+              marginTop: 10,
+              borderRadius: 5,
+              padding: 10,
+              borderWidth: 1,
+              borderColor: appColors.BORDER_COLOR,
+            }}>
+            {[
+              {name: 'Sex', value: ''},
+              {name: 'Male', value: 'Male'},
+              {name: 'Female', value: 'Female'},
+            ].map((model, i) => (
+              <Picker.Item key={i} label={model.name} value={model.value} />
+            ))}
+          </Picker>
+          <View style={{marginVertical: 10}}>
+            <Text style={{color: appColors.FOOTER_BODY_TEXT_COLOR}}>
+              Medication
+            </Text>
+            <TextInput
+              style={{
+                backgroundColor: appColors.WHITE,
+                marginTop: 10,
+                borderRadius: 5,
+                padding: 10,
+                borderWidth: 1,
+                borderColor: appColors.BORDER_COLOR,
+                color: appColors.BLACK,
+              }}
+              placeholder="Enter medication"
+              multiline={true}
+              onChangeText={(text: string) =>
+                setState({...state, medication: text})
+              }
+              value={state.medication}
+            />
+          </View>
+          <View style={{marginVertical: 10}}>
+            <Text style={{color: appColors.FOOTER_BODY_TEXT_COLOR}}>
+              Medical History
+            </Text>
+            <TextInput
+              style={{
+                backgroundColor: appColors.WHITE,
+                marginTop: 10,
+                borderRadius: 5,
+                padding: 10,
+                borderWidth: 1,
+                borderColor: appColors.BORDER_COLOR,
+                color: appColors.BLACK,
+              }}
+              placeholder="Medical History"
+              multiline={true}
+              onChangeText={(text: string) =>
+                setState({...state, medicalHistory: text})
+              }
+              value={state.medicalHistory}
+            />
+          </View>
+          <View style={{marginVertical: 10}}>
+            <Text style={{color: appColors.FOOTER_BODY_TEXT_COLOR}}>
+              Patient's email address
+            </Text>
+            <TextInput
+              style={{
+                backgroundColor: appColors.WHITE,
+                marginTop: 10,
+                borderRadius: 5,
+                padding: 10,
+                borderWidth: 1,
+                borderColor: appColors.BORDER_COLOR,
+                color: appColors.BLACK,
+              }}
+              placeholder="Enter email"
+              onChangeText={(text: string) => setState({...state, email: text})}
+              value={state.email}
+            />
+          </View>
+          <Pressable style={{marginTop: 10}} onPress={() => handleSubmit()}>
+            <View style={[commonAdminButtonContainerStyles]}>
+              <Text style={[commonAdminButtonTextStyles]}>Submit</Text>
             </View>
-            <View style={{marginVertical: 10}}>
-              <Text style={{color: appColors.FOOTER_BODY_TEXT_COLOR}}>
-                Medical History
-              </Text>
-              <TextInput
-                style={{
-                  backgroundColor: appColors.WHITE,
-                  marginTop: 10,
-                  borderRadius: 5,
-                  padding: 10,
-                  borderWidth: 1,
-                  borderColor: appColors.BORDER_COLOR,
-                  color: appColors.BLACK,
-                }}
-                placeholder="Medical History"
-                multiline={true}
-                onChangeText={(text: string) =>
-                  setState({...state, medicalHistory: text})
-                }
-                value={state.medicalHistory}
-              />
-            </View>
-            <View style={{marginVertical: 10}}>
-              <Text style={{color: appColors.FOOTER_BODY_TEXT_COLOR}}>
-                Patient's email address
-              </Text>
-              <TextInput
-                style={{
-                  backgroundColor: appColors.WHITE,
-                  marginTop: 10,
-                  borderRadius: 5,
-                  padding: 10,
-                  borderWidth: 1,
-                  borderColor: appColors.BORDER_COLOR,
-                  color: appColors.BLACK,
-                }}
-                placeholder="Enter email"
-                onChangeText={(text: string) =>
-                  setState({...state, email: text})
-                }
-                value={state.email}
-              />
-            </View>
-            <Pressable style={{marginTop: 10}} onPress={() => handleSubmit()}>
-              <View style={[commonAdminButtonContainerStyles]}>
-                <Text style={[commonAdminButtonTextStyles]}>Submit</Text>
-              </View>
-            </Pressable>
-          </ScrollView>
-        </View>
+          </Pressable>
+        </ScrollView>
       </View>
       <FullPageLoader isLoading={isLoading} />
     </KeyboardAvoidingView>
