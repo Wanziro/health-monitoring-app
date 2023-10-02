de9760514a39890290de392d2cc4c1834c9bc327
Fabrice Niyongabo <ninga.fab@gmail.com>
2023-10-02 15:12:47 +0200

    fix build issue in app.build.gradle after installing qrcode scanner

--- src/screens/nurses/scan-qr-code/index.tsx
+++ src/screens/nurses/scan-qr-code/index.tsx
@@ -1,26 +1,30 @@
-//@ts-nocheck
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import {Text, View, Alert} from 'react-native';
-import {Camera} from 'react-native-vision-camera';
+import QRCodeScanner from 'react-native-qrcode-scanner';
+import {RNCamera} from 'react-native-camera';
+
 const ScanQRCode = () => {
   const [qrCode, setQrCode] = useState<string | null>(null);
 
-  const onQRCodeRead = (qrCode: string) => {
+  const onQRCodeRead = (qrCode: any) => {
     setQrCode(qrCode);
+    Alert.alert('QR Code', qrCode);
   };
-
-  useEffect(() => {
-    if (qrCode !== null) Alert.alert('QR Code', qrCode);
-  }, [qrCode]);
 
   return (
     <View style={{flex: 1}}>
-      <Camera onQRCodeRead={onQRCodeRead} />
-      {qrCode && (
-        <Text style={{position: 'absolute', bottom: 20, right: 20}}>
-          {qrCode}
-        </Text>
-      )}
+      <QRCodeScanner
+        onRead={onQRCodeRead}
+        flashMode={RNCamera.Constants.FlashMode.torch}
+        reactivate={true} //to scann multiple times
+        reactivateTimeout={500}
+        // topContent={<Text>some tests</Text>}
+        // bottomContent={
+        //   <TouchableOpacity style={styles.buttonTouchable}>
+        //     <Text style={styles.buttonText}>OK. Got it!</Text>
+        //   </TouchableOpacity>
+        // }
+      />
     </View>
   );
 };
